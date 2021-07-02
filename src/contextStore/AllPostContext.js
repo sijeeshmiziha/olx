dbbd6c3b7014df6ba1f7da26bf3feb7b06cd3470
sijeeshmiziha <sijeeshmonbalan@gmail.com>
2021-07-02 14:19:36 +0530

    Add context providers for current post and all listings state

---
+++ src/contextStore/AllPostContext.js
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from 'react';
+
+export const AllPostContext = createContext(null);
+
+function ContextAllPost({ children }) {
+  const [allPost, setAllPost] = useState([]);
+  return (
+    <AllPostContext.Provider value={{ allPost, setAllPost }}>
+      {children}
+    </AllPostContext.Provider>
+  );
+}
+
+export default ContextAllPost;
---
+++ src/contextStore/PostContext.js
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from 'react';
+
+export const PostContext = createContext();
+
+const ContextPost = ({ children }) => {
+  const [postContent, setPostContent] = useState([]);
+
+  return (
+    <PostContext.Provider value={{ postContent, setPostContent }}>
+      {children}
+    </PostContext.Provider>
+  );
+};
+export default ContextPost;
