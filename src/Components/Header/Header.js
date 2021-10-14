d70cff2581b10625df554fe400991b6aadf84cb2
sijeeshmiziha <sijeeshmonbalan@gmail.com>
2021-10-14 11:27:32 +0530

    search feature added and almost finished to release the first vesion of olx-clone

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext,useState } from "react";
 import { useHistory } from "react-router";
-
+import { AllPostContext } from "../../contextStore/AllPostContext";
+import { PostContext } from "../../contextStore/PostContext";
 import "./Header.css";
 import OlxLogo from "../../assets/OlxLogo";
-import Search from "../../assets/Search";
+import SearchIcon from "../../assets/SearchIcon"
 import Arrow from "../../assets/Arrow";
 import SellButton from "../../assets/SellButton";
 import SellButtonPlus from "../../assets/SellButtonPlus";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contextStore/AuthContext";
 import { Firebase } from "../../firebase/config";
+import Search from "../Search/Search";
 function Header() {
-  const { user } = useContext(AuthContext);
+  const{allPost}=useContext(AllPostContext)
+  const{setPostContent}=useContext(PostContext)
   const history = useHistory();
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = allPost.filter((value) => {
+      return value.name.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+  const handleSelectedSearch=(value)=>{
+       setPostContent(value)
+       history.push("/view")
+
+  }
+  const handleEmptyClick=()=>{
+     alert("No items found.., please search by product name");
+  }
+  const { user } = useContext(AuthContext);
+  
   const logoutHandler = () => {
     Firebase.auth()
       .signOut()
@@ -27,26 +59,37 @@ function Header() {
           <OlxLogo></OlxLogo>
         </div>
         <div className="placeSearch">
-          <Search></Search>
-          <input type="text" />
-          <Arrow></Arrow>
+          <input type="text" 
+          placeholder="Search specific product..."
+          value={wordEntered}
+          onChange={handleFilter}
+        />{filteredData.length === 0 ? (
+          <div onClick={handleEmptyClick}> <SearchIcon /> </div>
+         ) : (
+           <div id="clearBtn"  onClick={clearInput} > <Arrow></Arrow></div>
+         )}
+          {filteredData.length !== 0 && (
+        <div className="dataResult-header">
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+              <div key={key} className="dataItem-header" onClick={()=>handleSelectedSearch(value)}>
+                <p>{value.name} </p>
+              </div>
+            );
+          })}
+        </div>
+      )}
+         
         </div>
         <div className="productSearch">
-          <div className="input">
-            <input
-              type="text"
-              placeholder="Find car,mobile phone and more..."
-            />
-          </div>
-          <div className="searchAction">
-            <Search color="#ffffff"></Search>
-          </div>
+          <Search />
         </div>
+        
         <div className="language">
           <span> ENGLISH </span>
           <Arrow></Arrow>
         </div>
-        <div className="loginPage"> 
+        <div className="loginPage">
           {user ? (
             user.displayName
           ) : (
@@ -56,7 +99,12 @@ function Header() {
           )}
           <hr />
         </div>
-        {user && <span onClick={logoutHandler} className="logout-span">Logout</span>}
+        {user && (
+          <span onClick={logoutHandler} className="logout-span">
+            Logout
+          </span>
+        )}
+        
         <Link to="/create">
           {" "}
           <div className="sellMenu">
--- src/Routes/MainRoutes.js
+++ src/Routes/MainRoutes.js
@@ -9,6 +9,7 @@ import ViewMore from '../Pages/ViewMore'
 
 
 
+
 function MainRoutes() {
     return (
        <Router>
@@ -30,6 +31,7 @@ function MainRoutes() {
            <Route path="/viewmore">
                <ViewMore/>
            </Route>
+         
            
 
            
